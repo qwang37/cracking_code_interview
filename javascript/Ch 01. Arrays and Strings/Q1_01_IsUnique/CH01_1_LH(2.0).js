ddb7b0eaae229a1b72ea8ed679f37b159c131be8
Lihong Han <36283184+hlhNicola@users.noreply.github.com>
2018-06-16 15:33:21 -0500

    Update CH01_1_LH(2.0).js

--- javascript/Ch 01. Arrays and Strings/Q1_01_IsUnique/CH01_1_LH(2.0).js
+++ javascript/Ch 01. Arrays and Strings/Q1_01_IsUnique/CH01_1_LH(2.0).js
@@ -1,3 +1,4 @@
+//Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
 function Unique(str){
     var Arr = str.split("");
     var newArr = [];
