a0d189eaa512bc4be4b7d0fd05419f3a08480188
Lihong Han <36283184+hlhNicola@users.noreply.github.com>
2018-06-16 15:35:18 -0500

    Update CH01_2LH(1.0).js

--- javascript/Ch 01. Arrays and Strings/Q2_01_C-style/CH01_2LH(1.0).js
+++ javascript/Ch 01. Arrays and Strings/Q2_01_C-style/CH01_2LH(1.0).js
@@ -1,3 +1,5 @@
+//Write code to reverse a C-Style String (C-String means that “abcd” is represented as five characters, 
+//including the null character )
 function C_style(str){
     return str.split("").reverse().slice(1).join("") + " ";
 
