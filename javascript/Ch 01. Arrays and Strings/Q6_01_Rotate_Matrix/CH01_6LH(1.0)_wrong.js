3d64f4d1c019ba89c62f0c0ad67bd99037b64730
Lihong Han <36283184+hlhNicola@users.noreply.github.com>
2018-06-16 15:14:31 -0500

    CH01_6LH(1.0)_wrong.js
    I thought the original top has been saved properly, no idea why it keeps changing.

---
+++ javascript/Ch 01. Arrays and Strings/Q6_01_Rotate_Matrix/CH01_6LH(1.0)_wrong.js
@@ -0,0 +1,28 @@
+var Matrix = [[1,2,3,4], [5,6,7,8], [9,10,11,12],[13,14,15,16]];
+
+function rotation(matrix){
+    var cycle = Math.ceil(matrix.length/2);
+    for(var i = 0; i < cycle; i++){
+        var last = matrix.length - 1 - i;
+
+        for(var j = i; j < last+1; j++){
+            //console.log(j)
+            var offset = j - i;
+            var top = matrix[i][last-offset];//save top
+            var left = matrix[j][i];//save left
+            var bottom = matrix[last][j];//save bottom
+            var right = matrix[last-offset][last];//save right
+            //console.log(top);???I thought the original top,left,right and bottom have been saved. Why it still change by following code? It suppose to be execute first！
+
+            matrix[i][last-offset] = left;//left-->top
+            matrix[j][i] = bottom;//bottom-->left
+            matrix[last][j] = right;//right-->bottom
+            matrix[last-offset][last] = top;//top-->right
+        }
+    }
+
+    return matrix;
+}
+
+
+console.log(rotation(Matrix))
