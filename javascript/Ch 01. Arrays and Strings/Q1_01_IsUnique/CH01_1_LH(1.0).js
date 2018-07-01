a28b75b5dcec78aa3021ebaeaf7ec073ed8c5fd3
Lihong Han <littlefly14@gmail.com>
2018-07-01 14:36:10 -0500

    sdf

--- javascript/Ch 01. Arrays and Strings/Q1_01_IsUnique/CH01_1_LH(1.0).js
+++
@@ -1,5 +0,0 @@
-//Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
-function unique(str){
-  return [...new Set(str.split(""))] === str.split("");
-}
-unique("happy");
--- javascript/Ch 01. Arrays and Strings/Q5_01_Replace/CH01_5LH(1.0).js
+++
@@ -1,5 +0,0 @@
-//Write a method to replace all spaces in a string with ‘%20’
-function replace(str){
-   return str.split(" ").join("20%");
-}
-console.log(replace("I am good."))
